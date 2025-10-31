e29a150eecb80d2c3e7d0467597cd6eec4aece83
Ganesh Bommidi <bommidiganesh27@gmail.com>
2025-10-31 11:24:30 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Firestore has alre

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,49 +3,47 @@
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore, enableIndexedDbPersistence } from 'firebase/firestore'
+import { getFirestore, enableIndexedDbPersistence, Firestore } from 'firebase/firestore'
+
+let firestoreInstance: Firestore | null = null;
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
-  if (!getApps().length) {
-    // Important! initializeApp() is called without any arguments because Firebase App Hosting
-    // integrates with the initializeApp() function to provide the environment variables needed to
-    // populate the FirebaseOptions in production. It is critical that we attempt to call initializeApp()
-    // without arguments.
-    let firebaseApp;
+  let firebaseApp;
+  if (getApps().length) {
+    firebaseApp = getApp();
+  } else {
     try {
       // Attempt to initialize via Firebase App Hosting environment variables
       firebaseApp = initializeApp();
     } catch (e) {
-      // Only warn in production because it's normal to use the firebaseConfig to initialize
-      // during development
       if (process.env.NODE_ENV === "production") {
         console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
       }
       firebaseApp = initializeApp(firebaseConfig);
     }
-
-    return getSdks(firebaseApp);
   }
 
-  // If already initialized, return the SDKs with the already initialized App
-  return getSdks(getApp());
+  return getSdks(firebaseApp);
 }
 
 export function getSdks(firebaseApp: FirebaseApp) {
-  const firestore = getFirestore(firebaseApp);
-  enableIndexedDbPersistence(firestore).catch((err) => {
-    if (err.code == 'failed-precondition') {
-      console.warn('Firestore persistence failed: Multiple tabs open, persistence can only be enabled in one tab at a time.');
-    } else if (err.code == 'unimplemented') {
-      console.warn('Firestore persistence failed: The current browser does not support all of the features required to enable persistence.');
-    }
-  });
+  if (!firestoreInstance) {
+      const db = getFirestore(firebaseApp);
+      enableIndexedDbPersistence(db).catch((err) => {
+        if (err.code == 'failed-precondition') {
+          console.warn('Firestore persistence failed: Multiple tabs open, persistence can only be enabled in one tab at a time.');
+        } else if (err.code == 'unimplemented') {
+          console.warn('Firestore persistence failed: The current browser does not support all of the features required to enable persistence.');
+        }
+      });
+      firestoreInstance = db;
+  }
   
   return {
     firebaseApp,
     auth: getAuth(firebaseApp),
-    firestore: firestore
+    firestore: firestoreInstance
   };
 }
 
