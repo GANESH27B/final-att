6519b5acd0ef149bd6489cd62281c8e57e775e4b
Ganesh Bommidi <bommidiganesh27@gmail.com>
2025-10-31 12:21:07 +0000

    when i to can this web to app in the webtoapp site when i install the ap

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -6,6 +6,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore, enableIndexedDbPersistence, Firestore } from 'firebase/firestore'
 
 let firestoreInstance: Firestore | null = null;
+let persistenceEnabled = false;
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
@@ -30,13 +31,16 @@ export function initializeFirebase() {
 export function getSdks(firebaseApp: FirebaseApp) {
   if (!firestoreInstance) {
       const db = getFirestore(firebaseApp);
-      enableIndexedDbPersistence(db).catch((err) => {
-        if (err.code == 'failed-precondition') {
-          console.warn('Firestore persistence failed: Multiple tabs open, persistence can only be enabled in one tab at a time.');
-        } else if (err.code == 'unimplemented') {
-          console.warn('Firestore persistence failed: The current browser does not support all of the features required to enable persistence.');
-        }
-      });
+      if (!persistenceEnabled) {
+          enableIndexedDbPersistence(db).catch((err) => {
+            if (err.code == 'failed-precondition') {
+              console.warn('Firestore persistence failed: Multiple tabs open, persistence can only be enabled in one tab at a time.');
+            } else if (err.code == 'unimplemented') {
+              console.warn('Firestore persistence failed: The current browser does not support all of the features required to enable persistence.');
+            }
+          });
+          persistenceEnabled = true;
+      }
       firestoreInstance = db;
   }
   
