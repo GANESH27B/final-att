cc8ce051b34423d7b5b1627925a8a894016b398b
Ganesh Bommidi <bommidiganesh27@gmail.com>
2025-11-03 18:09:27 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Missing or insuffi

--- src/app/(app)/dashboard/faculty/page.tsx
+++ src/app/(app)/dashboard/faculty/page.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 import {
   Card,
@@ -127,7 +128,7 @@ export default function FacultyDashboardPage() {
   return (
     <div className="space-y-4">
        <h1 className="text-2xl font-bold tracking-tight font-headline">Faculty Dashboard</h1>
-      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-3">
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">My Classes</CardTitle>
@@ -220,5 +221,3 @@ export default function FacultyDashboardPage() {
     </div>
   );
 }
-
-    
