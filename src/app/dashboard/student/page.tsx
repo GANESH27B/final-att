658f19f41d7192fd58bcd42a9d01b1694925a4d0
Ganesh Bommidi <bommidiganesh27@gmail.com>
2025-11-08 09:45:32 +0000

    show dashboard name (_for element main_)

--- src/app/dashboard/student/page.tsx
+++ src/app/dashboard/student/page.tsx
@@ -3,7 +3,7 @@
 export default function StudentDashboardPage() {
   return (
     <div className="space-y-6">
-      
+      <h1 className="text-2xl font-bold tracking-tight font-headline">Student Dashboard</h1>
     </div>
   );
 }
